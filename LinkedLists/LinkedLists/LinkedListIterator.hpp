52f9ac1b9bf232a403d1e74895f288c501340673
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-07-09 17:25:12 -0700

    linked list iterator

--- LinkedLists/LinkedLists/LinkedListIterator.hpp
+++ LinkedLists/LinkedLists/LinkedListIterator.hpp
@@ -37,4 +37,38 @@ private:
     nodeType<Type> *current;
 };
 
+
+template<class Type>
+linkedListIterator<Type>:: linkedListIterator(){
+    current = NULL;
+}
+
+
+template<class Type>
+linkedListIterator<Type>:: linkedListIterator(nodeType<Type> *ptr) {
+    current = ptr;
+}
+
+template<class Type>
+Type linkedListIterator<Type>::  operator*() {
+    return current-> info;
+}
+
+template<class Type>
+linkedListIterator<Type> linkedListIterator<Type>:: operator++() {
+    current = current-> link;
+    return *this;
+}
+
+template<class Type>
+bool linkedListIterator<Type>::  operator ==(const linkedListIterator<Type>& right) const {
+    return (current == right.current);
+}
+
+template<class Type>
+bool linkedListIterator<Type>::  operator!= (const linkedListIterator<Type>& right) const {
+    return (current != right.current);
+}
+
+
 #endif /* LinkedListIterator_hpp */
